3b2ee762c9b515906735934999a97b1f95f41f81
utsav505 <utsavp505@gmail.com>
2020-08-13 19:51:28 +0530

    slingshot added

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.image1 = loadImage("sprites/sling1.png");
+        this.image2 = loadImage("sprites/sling2.png");
+        this.image3 = loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +20,30 @@ class SlingShot{
     }
 
     display(){
+        image(this.image1,200,20);
+        image(this.image2,170,20);
+        
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            strokeWeight(10);
+            push();
+            stroke(48,22,8);
+            if(pointA.x<220){
+                line(pointA.x-20, pointA.y, pointB.x-15, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+                imageMode(CENTER);
+                image(this.image3,pointA.x-20,pointA.y+5,15,30);
+            }else{
+                line(pointA.x+25, pointA.y, pointB.x-15, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+20, pointB.y);
+                imageMode(CENTER);
+                image(this.image3,pointA.x+25,pointA.y+5,15,30);
+
+
+            }
+            pop();
         }
     }
     
